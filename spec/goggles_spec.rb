d30e73bbbb6e5a414cd5405e52bafd64c18bbc57
Johnson Denen <jdenen@manta.com>
2014-01-18 01:51:08 -0500

    add tests for scriptless config and refine specs

--- spec/goggles_spec.rb
+++ spec/goggles_spec.rb
@@ -4,18 +4,38 @@ describe Goggles do
   Given(:config_path) { "./spec/support/configs" }
   Given(:images) { Dir.glob("./spec/support/results/*/*.png") }
   Given(:diffs) { Dir.glob("./spec/support/results/*/*diff.png") }
+  Given(:datas) { Dir.glob("./spec/support/results/*/*data.txt") }
 
-  context "Google search at 1024x768" do
-    Given(:config) { "#{config_path}/test_config_1024.yml" }
-    When { Goggles.swim(config) }
-    Then { images.size.should == 6 }
-    And { diffs.size.should == 2 }
+  context "swimming with one script at multiple sizes" do
+    describe "taking screenshots" do
+      Given(:sizes_config) { "#{config_path}/test_config_1024_600.yml" }
+      When { Goggles.swim(sizes_config) }
+      Then { images.size.should == 12 }
+    end
+
+    describe "generating diff images" do
+      Then { diffs.size.should == 4 }
+    end
+
+    describe "generating diff data" do
+      Then { datas.size.should == 4 }
+    end
   end
 
-  context "Google search at 1024x768 and 600x768" do
-    Given(:config) { "#{config_path}/test_config_1024_600.yml" }
-    When { Goggles.swim(config) }
-    Then { images.size.should == 12 }
-    And { diffs.size.should == 4 }
+  context "swimming against multiple paths without a script" do
+    describe "taking screenshots" do
+      Given(:scriptless_config) { "#{config_path}/test_config_scriptless.yml" }
+      When { Goggles.swim(scriptless_config) }
+      Then { images.size.should == 6 }
+    end
+
+    describe "generating diff images" do
+      Then { diffs.size.should == 2 }
+    end
+
+    describe "generating diff data" do
+      Then { datas.size.should == 2 }
+    end
   end
+
 end
